c0122daecfb9c89dbdb8b472226582160de6adf9
Seth <sba2126@gmail.com>
2022-10-08 10:55:48 -0700

    Add op<< for easy printing

--- Week3_Dijkstra/graph.h
+++ Week3_Dijkstra/graph.h
@@ -22,6 +22,7 @@ namespace dijkstra {
             int get_id() const;
             void add_edge(std::shared_ptr<Edge> edge);
             const std::vector<std::pair<int, double>> get_edges() const;
+            friend std::ostream &operator<<(std::ostream &os, Vertex const &vert);
         private:
             int id;
             std::vector<std::shared_ptr<Edge>> conn_list;
@@ -34,11 +35,28 @@ namespace dijkstra {
             const Vertex &get_b() const;
             const Vertex &get_other(const Vertex &thing) const;
             double get_weight() const;
+            friend std::ostream &operator<<(std::ostream &os, Edge const &edge)
+            {
+                os << "Edge: " << edge.vertex_a->get_id() << " <-> " << edge.vertex_b->get_id()
+                    << ", weight" << edge.weight;
+                return os;
+            }
         private:
             std::shared_ptr<Vertex> vertex_a;
             std::shared_ptr<Vertex> vertex_b;
             double weight;
     };
+
+    // Need to define this below Edge, since the compiler doesn't know what Edge is when Vertix is declared
+    std::ostream &operator<<(std::ostream &os, Vertex const &vert)
+    {
+        os << "Vertex " << vert.id << "\n";
+        for (auto edge: vert.conn_list) {
+            os << " - " << edge->get_other(vert).get_id() << " (weight=" << edge->get_weight() << ")\n";
+        }
+        return os;
+    }
+
 
     class Path {
         public:
@@ -47,6 +65,15 @@ namespace dijkstra {
             void push_front(int id);
             void push_back(int id);
             const std::list<int> get_path() const;
+            friend std::ostream &operator<<(std::ostream &os, Path const &path)
+            {
+                os << "Path: weight=" << path.weight << "\n - ";
+                for (auto elem_i : path.path) {
+                    os << elem_i << " -> ";
+                }
+                os << std::endl;
+                return os;
+            }
         private:
             double weight;
             std::list<int> path;
@@ -56,6 +83,16 @@ namespace dijkstra {
         public:
             Graph(int n_nodes, double density, double dist_min, double dist_max);
             const Path find_path(int start, int end) const;
+            friend std::ostream &operator<<(std::ostream &os, Graph const &graph)
+            {
+                os << "Graph: n_nodes=" << graph.n_nodes << ", density=" << graph.density
+                    << ", dist=(" << graph.dist_min << ", " << graph.dist_max << ")\n";
+                for (auto vert : graph.vertex_list) {
+                    os << *vert;
+                }
+                os << std::endl;
+                return os;
+            }
         private:
             int n_nodes;
             double density;
